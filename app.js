9558f5662eadbe76441c916df90bbf166bbabc85
samet sengel <sametsengel@gmail.com>
2018-04-26 14:43:19 +0300

    veri seti eklendi

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ let Auth = require('./auth/AuthController');
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
-    res.header('Access-Control-Allow-Headers', 'Content-Type')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token')
     next()
 })
 
